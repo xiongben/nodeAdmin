3f8b951d749961e34588dd918a4c7d0ea5818dcc
xiongben <1633387024@qq.com>
2017-07-27 23:53:32 +0800

    add

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,47 @@ var express = require('express');
 var router = express.Router();
 var url=require("url");
 var http = require("http");
+var mysql = require('mysql');
 /* GET users listing. */
+
+var info;
 router.get('/', function(req, res, next) {
   var pathname =req.query;
   res.send('respond with a resource new'+pathname.hello);
   res.end();
+});
+
+var connection = mysql.createConnection({
+  host     : 'bdm114585403.my3w.com',
+  user     : 'bdm114585403',
+  password : 'xb19930319',
+  database : 'bdm114585403_db',
+  insecureAuth: true
+});
+
+connection.connect();
+var  sql = 'SELECT * FROM mytest';
+
+connection.query(sql, function (err, results, fields) {
+  if(err){
+          console.log('[SELECT ERROR] - ',err.message);
+          return;
+        }
+  info=results;
+  console.log(results);
+});
+
+router.get('/test', function(req, res, next) {
+  var pathname =req.query;
+  var user=pathname.user;
+  if(user == 'xiongben'){
+    res.send(info);
+    res.end();
+  }else{
+    res.send('respond with a resource new'+pathname.hello);
+    res.end();
+  }
+
 });
 
 module.exports = router;
